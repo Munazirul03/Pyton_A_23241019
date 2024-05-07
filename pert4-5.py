55e95ab5c0e94e490995e8b7c86e46588671e6df
Munazirul03 <145947248+Munazirul03@users.noreply.github.com>
2024-05-07 12:02:28 +0800

    Add files via upload

---
+++ pert4-5.py
@@ -0,0 +1,49 @@
+# inputan
+# aritmatika + - x :
+
+#coba = input("masukkan nama :")
+#print(coba)
+
+
+# bilangan ranodm = input ("masukkan bilangan acak :")
+# print bilangan random
+
+#ARITMATIKA
+y=9
+x=3
+
+#penjumlahan +
+hasil = x + y
+print(x, '+' , y, '=', hasil)
+
+#pengurangan -
+hasil = x - y
+print(x, '-' , y, '=', hasil)
+
+#perkalian *
+hasil = x * y
+print(x, 'x' , y, '=', hasil)
+
+#pembagian /
+hasil = x / y
+print(x, ':' , y, '=', hasil)
+
+#perpangkatan **
+hasil = x ** y
+print(x, '^' , y, '=', hasil)
+
+#modulus %
+hasil = x % y
+print(x, 'mod' , y, '=', hasil)
+
+#floor devision //
+hasil = x // y
+print(x, 'floor' , y, '=', hasil)
+
+#skala prioritas (), **, */:, +/-
+x = 3
+y = 4
+z = 5
+hasil = ( x ** y * (z + z) / y - z % x // y)
+print(hasil)
+print( x, '**', y, 'x', z, '+', x, '/', y, '-', z, '%',)
