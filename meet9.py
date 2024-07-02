b421976653e534413b373c5333e52227baced72b
Munazirul03 <145947248+Munazirul03@users.noreply.github.com>
2024-07-02 08:46:45 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,30 @@
+#pengulangan
+
+bil = [1, 2, 3, 4, 5, 6]
+print(bil)
+
+
+#contoh menggunakan list
+for i in bil:
+    print(f'nilai i sekarang adalah =>{i}')
+
+
+print(20*'=')
+
+bil2 = range(1, 6)
+for i in bil2:
+    print(f'nilai i sekarang adalah =>{i}')
+
+
+print(20*'=')
+
+for i in range(1, 101):
+    print(f'nomor {i}')
+
+print(20*'=')
+
+
+ror = range(1, 11)
+for i in ror:
+    print(f'nilai i sekarang adalah {i}')
+    print('rorrrrrrrrr')
